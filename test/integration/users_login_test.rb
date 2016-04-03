ebc40af4735f82f63ffed52078117b5e86b6842e
theroncross <theroncross@gmail.com>
2016-04-03 15:19:11 +0000

    Add authorization methods and tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -32,8 +32,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,       count: 0
-    assert_select "a[href=]", user_path(@user),   count: 0
+    assert_select "a[href=?]", logout_path,        count: 0
+    assert_select "a[href=?]", user_path(@user),   count: 0
   end
   
   test "login with remembering" do
